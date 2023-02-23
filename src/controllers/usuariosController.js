4022b33a84a5345e4726a01c52d57b4e006b3038
Guilherme-07062002 <guilherme762002@gmail.com>
2023-02-23 15:04:33 -0300

    toques finais

--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -4,23 +4,15 @@ const alunoPersonal = require('../models/alunoPersonal')
 const avaliacaoPersonal = require('../models/avaliacaoPersonal')
 const cadastroTreino = require("../models/cadastroTreino");
 
+// Conforme pedido foi utilizado, o json web token para a autenticação
 const jwt = require("jsonwebtoken");
 
 const SECRET = 'secret'
+// A blacklist serve para que um usuário que fez logout não possa acessar o site, antes que ele faça login novamente
 const blacklist = []
 
 module.exports = {
 
-    async all(request, response) {
-        try {
-            console.log('Acesso autorizado')
-            const usuarios = await Usuarios.findAll();
-            response.status(200).json(usuarios);
-        } catch (error) {
-            console.log(error);
-            response.status(400).send(error);
-        }
-    },
     async autenticate(request, response) {
         try {
             const email = request.body.email;
@@ -247,65 +239,4 @@ module.exports = {
             response.status(400).json("Somente um personal trainer pode visualizar os treinos cadastrados.")
         }
     }
-
-    // ,
-    // async showtreino(request, response) {
-    //     try {
-    //         console.log('Acesso autorizado')
-    //         const usuarios = await registroTreino.findAll();
-    //         response.status(200).json(usuarios);
-    //     } catch (error) {
-    //         console.log(error);
-    //         response.status(400).send(error);
-    //     }
-    // }
-    //,
-    // async one(request, response) {
-    //     try {
-    //         const id = request.params.id;
-    //         const usuarios = await Usuarios.findOne({ where: { id } });
-    //         if (!usuarios) {
-    //             return response.status(400).json("Usuário não encontrado.");
-    //         }
-    //         response.status(200).json(usuarios);
-    //     } catch (error) {
-    //         console.log(error);
-    //         response.status(400).send(error);
-    //     }
-    // },
-    // async update(request, response) {
-    //     try {
-    //         const { nome, email, documento, tipo, senha } = request.body;
-    //         const id = request.params.id;
-    //         const usuarios = await Usuarios.findOne({ where: { id } });
-
-    //         if (!usuarios) {
-    //             return response.status(400).json("Usuário não encontrado.");
-    //         }
-    //         usuarios.nome = nome;
-    //         usuarios.email = email;
-    //         usuarios.documento = documento;
-    //         usuarios.tipo = tipo;
-    //         usuarios.senha = senha;
-
-    //         await usuarios.save();
-    //         response.status(200).json("Dados do usuário atualizados.");
-    //     } catch (error) {
-    //         console.log(error);
-    //         response.status(400).send(error);
-    //     }
-    // },
-    // async delete(request, response) {
-    //     try {
-    //         const id = request.params.id;
-    //         const usuarios = await Usuarios.destroy({ where: { id } });
-    //         if (!usuarios) {
-    //             return response.status(400).json("Usuário não encontrado.");
-    //         }
-    //         response.status(200).json("Usuário removido.");
-    //     } catch (error) {
-    //         console.log(error);
-    //         response.status(400).send(error);
-    //     }
-    // }
 };
--- src/routes/usuario_routes.js
+++ src/routes/usuario_routes.js
@@ -1,33 +1,33 @@
 const express = require("express");
 const { verifyJWT, autenticate, logout, storeWorkout, deleteAccount, cadastrarPersonal, cadastrarAluno, matricularAluno, avaliarPersonal, cadastrarTreino, listarTreinos } = require("../controllers/usuariosController");
-const usuariosController = require("../controllers/usuariosController");
 
 const router = express.Router();
 
-
-// Rota de teste
-//router.route("/teste").get(verifyJWT, usuariosController.showtreino)
-//router.route('/create').post(usuariosController.create)
-// Login
-router.route("/auth").get(autenticate)
-// Logout
-router.route("/logout").get(logout)
-// Registro de treino
-router.route('/store/workout').post(verifyJWT, storeWorkout)
-// Deletar conta de usuário
-router.route('/account/delete').get(verifyJWT, deleteAccount)
 // Cadastrar aluno
 router.route('/register/student').post(cadastrarAluno)
 // Cadastrar personal trainer
 router.route('/register/personal').post(cadastrarPersonal)
-// Matricular usuário como aluno de um determinado personal
-router.route('/personal/student').post(verifyJWT, matricularAluno)
+
+// Login
+router.route("/auth").get(autenticate)
+// Logout
+router.route("/logout").get(logout)
+
+// Os endpoints abaixo são voltados para os usuários do tipo ALUNO
+// Registro de treino
+router.route('/store/workout').post(verifyJWT, storeWorkout)
+// Deletar conta de usuário
+router.route('/account/delete').delete(verifyJWT, deleteAccount)
 // Permitir que usuário avalie um personal
 router.route('/personal/:personal_id/rating').post(verifyJWT, avaliarPersonal)
+
+
+// Os endpoints abaixo são voltados para os usuários do tipo PERSONAL
+// Matricular usuário como aluno de um determinado personal
+router.route('/personal/student').post(verifyJWT, matricularAluno)
 // Permitir que personal trainer cadastre treinos
 router.route('/personal/workout').post(verifyJWT, cadastrarTreino)
 // Consultar treinos cadastrados pelos personal 
-router.route('/personal/workout').get(listarTreinos)
-// router.route("/:id").get(usuariosController.one).put(usuariosController.update).delete(usuariosController.delete)
+router.route('/personal/workout').get(verifyJWT, listarTreinos)
 
 module.exports = router;
